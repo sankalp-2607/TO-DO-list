c3de2e2588a458ed44f5cfffab0a322a63349ec2
sankalp-2607 <ssankalp702@gmail.com>
2024-12-22 11:45:44 +0530

    cpp file added

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int c,a,b;
+    cout<<"enter a: ";
+    cin>>a;
+    cout<<"enter b: ";
+    cin>>b;
+    c=a+b;
+    cout <<"sum is: "<<c;
+
+    return 0;
+}
