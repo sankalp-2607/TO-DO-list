3fb6bfd38658e4839db1cdd34abbbd394f69d2b5
sankalp-2607 <ssankalp702@gmail.com>
2024-12-24 14:00:48 +0530

    change js

--- script.js
+++ script.js
@@ -6,8 +6,31 @@ function addTask(){
         alert("You must write something! ");
     }
     else{
-        let li = document.createElement(li);
+        let li = document.createElement("li");
         li.innerHTML =inputBox.value;
-        listContainer.appendChild(li)
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
     }
+    inputBox.value = "";
+    saveData()
+}
+
+listContainer.addEventListener("click" ,function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.atrget.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML =localStorage.getItem("data")
 }
